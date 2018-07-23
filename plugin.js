615f6d758ad436ba5ec13d2f65300b4b83d6dffc
liasica <magicrolan@gmail.com>
2018-07-23 23:49:15 +0800

    remove $notify

--- plugin.js
+++ plugin.js
@@ -21,5 +21,5 @@ else {
 export default function (ctx, inject, c, d) {
   izitoast.settings(<%= serialize(options) %>)
   inject('izitoast', izitoast)
-  inject('notify', izitoast)
+  // inject('notify', izitoast)
 }
